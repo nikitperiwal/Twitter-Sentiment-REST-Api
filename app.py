d34695c2b22d72c3c7fcd1e8c32fd3064a4de602
Nikit <nikit.periwal@gmail.com>
2022-02-06 19:11:46 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def getTweet(keywords: str,
 
 
 if __name__ == "__main__":
-    # Run app with uvicorn with port and host specified. Host needed for docker port mapping
+    # Run app with uvicorn with port and host specified. Host needed for docker port mapping.
     uvicorn.run(app, port=8000, host="127.0.0.1")
